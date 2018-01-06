ac00848a3f9c8e218cdb0820eefb63fbf1002ae0
HaGaoKo_M510 <wsliuwenxiang@qq.com>
2018-01-06 23:11:44 +0800

    playerAutoMove的add

--- Assets/Scripts/Player/PlayerAutoMove.cs
+++ Assets/Scripts/Player/PlayerAutoMove.cs
@@ -1,16 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerAutoMove : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    NavMeshAgent agent;
+
+    public float minDistance = 2.5f;
+
+    //public Transform testPos;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (agent.enabled)
+        {
+            if (agent.remainingDistance < minDistance)
+            {
+                agent.isStopped = true;
+                agent.enabled = false;
+            }
+        }
+
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    SetDestination(testPos.position);
+        //}
+    }
+
+    /// <summary>设置目标</summary>
+    public void SetDestination(Vector3 targetPos)
+    {
+        agent.enabled = true;
+        agent.SetDestination(targetPos);
+    }
 }
